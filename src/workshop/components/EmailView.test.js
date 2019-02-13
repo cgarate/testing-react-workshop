cb329927b034841448ea9fa28475ac7bd35c74ab
Carlos Garate <carlos.garate@rangle.io>
2019-02-12 20:34:49 -0500

    Step 1 EmailListItem tests completed

---
+++ src/workshop/components/EmailView.test.js
@@ -0,0 +1,27 @@
+describe('prop rendering', () => {
+  describe('isSelected', () => {
+    it('includes "email-list-item--selected" class on container when `isSelected` prop is true', () => {
+
+    });
+
+
+    it('excludes "email-list-item--selected" class on container when `isSelected` prop is false', () => {
+
+    });
+  });
+
+  describe('email', () => {
+    it('includes "email-list-item--unread" class on container & hides "mark unread" button when `email.read` property is false', () => {
+
+    });
+
+
+    it('excludes "email-list-item--unread" class on container & hides "mark unread" button when `email.read` property is true', () => {
+
+    });
+
+    it('includes "mark unread" button when both `email.read` property and `isSelected` prop are true', () => {
+
+    });
+  });
+});
